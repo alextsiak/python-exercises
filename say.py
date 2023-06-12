b000586ece16157cd207b1e673fb1d2af5131107
Alex Tsiakalou <35167274+alextsiak@users.noreply.github.com>
2023-06-12 17:03:58 +0300

    Update exercise

--- say.py
+++ say.py
@@ -1,23 +1,21 @@
 """
-WORK IN PROGRESS
+WORK IN PROGRESS    
 say.py - Given a number from 0 to 999,999,999,999, spells out that number in English.
 """
-basics = {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten", "11": "eleven", "12": "twelve", "13": "thirteen", "14": "fourteen", "15": "fifteen", "16": "sixteen", "17": "seventeen", "18": "eighteen", "19": "nineteen"}
+basics = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
-decs = {"20": "twenty", "30": "thirty", "40": "forty", "50": "fifty", "60": "sixty", "70": "seventy", "80": "eighty", "90": "ninety"}
+tens = {20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety"}
 
-def say():
-    number = input("Please enter a number: ")
-
-    #basic cases: 0-99
-    if len(number) == 1:
-        print(basics[number])
-    elif len(number) == 2:
-        if number in basics.keys():
-            print(basics[number])
-        if number in decs.keys():
-            print(decs[number])
+def say(number):
+    if number < 0:
+        raise ValueError("Input out of range!")
+    elif number < 20:
+        return basics[number]
+    elif number in tens.keys():
+        return tens[number]
+    elif number < 100:
+        return f"{say(number-number%10)}-{say(number%10)}"
     else:
         raise ValueError("Input out of range!")
 
-say()
+print(say(95))
